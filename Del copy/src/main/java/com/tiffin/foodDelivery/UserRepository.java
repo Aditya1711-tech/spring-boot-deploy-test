f205021f5f5ee536ba4bf5d2d2ed7bfbf7780f9e
Aditya1711-tech <adityakaneriya1711@gmail.com>
2024-12-20 09:08:03 +0530

    Add temp Project on feature-2 branch

---
+++ Del copy/src/main/java/com/tiffin/foodDelivery/UserRepository.java
@@ -0,0 +1,8 @@
+package com.tiffin.foodDelivery;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    // Custom query methods can be added here
+}
+
---
+++ Del copy/src/main/java/com/tiffin/foodDelivery/UserService.java
@@ -0,0 +1,30 @@
+package com.tiffin.foodDelivery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUserById(String id) {
+        userRepository.deleteById(id);
+    }
+}
+
